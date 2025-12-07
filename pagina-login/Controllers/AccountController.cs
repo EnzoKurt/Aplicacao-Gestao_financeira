39eb891f882a7c9a2961cc1ea6c939e35e6a0bce
Gustavo Viana <128104739+gustavo-henrique-viana@users.noreply.github.com>
2025-12-07 19:48:27 -0300

    Create AccountController.cs

---
+++ pagina-login/Controllers/AccountController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace PAGINA_LOGIN.Controllers
+{
+    public class AccountController : Controller
+    {
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Register()
+        {
+            return View();
+        }
+    }
+}
